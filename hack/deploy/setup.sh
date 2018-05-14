076a5d68cc5631d3cd83644b660454f034d3d245
Maruf Tuhin <maruf.2hin@gmail.com>
2018-05-14 09:47:14 +0600

    Refactored E2E testing to support E2E testing with admission webhook in cloud (#38)

---
+++ hack/deploy/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -eou pipefail
+
+GOPATH=$(go env GOPATH)
+
+REPO_ROOT="$GOPATH/src/github.com/kubedb/mongodb"
+CLI_ROOT="$GOPATH/src/github.com/kubedb/cli"
+
+pushd $REPO_ROOT
+
+source "$REPO_ROOT/hack/deploy/settings"
+source "$REPO_ROOT/hack/libbuild/common/lib.sh"
+
+export APPSCODE_ENV=${APPSCODE_ENV:-prod}
+export KUBEDB_SCRIPT="curl -fsSL https://raw.githubusercontent.com/kubedb/cli/0.8.0-beta.3/"
+
+
+if [ "$APPSCODE_ENV" = "dev" ]; then
+    detect_tag ''
+    export KUBEDB_SCRIPT="cat "
+    export CUSTOM_OPERATOR_TAG=$TAG
+    echo ""
+
+    if [[ ! -d $CLI_ROOT ]]; then
+        echo ">>> Cloning cli repo"
+        git clone -b $CLI_BRANCH https://github.com/kubedb/cli.git "${CLI_ROOT}"
+        pushd $CLI_ROOT
+    else
+        pushd $CLI_ROOT
+        detect_tag ''
+        if [[ $git_branch != $CLI_BRANCH ]]; then
+            git fetch --all
+            git checkout $CLI_BRANCH
+        fi
+    fi
+fi
+
+${KUBEDB_SCRIPT}hack/deploy/kubedb.sh | bash -s -- --operator-name=mg-operator "$@"
--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -37,6 +37,7 @@ func init() {
 	flag.StringVar(&storageClass, "storageclass", "standard", "Kubernetes StorageClass name")
 	flag.StringVar(&framework.DockerRegistry, "docker-registry", "kubedb", "User provided docker repository")
 	flag.StringVar(&framework.ExporterTag, "exporter-tag", "canary", "Tag of kubedb/operator used as exporter")
+	flag.BoolVar(&framework.SelfHostedOperator, "selfhosted-operator", false, "Enable this for provided controller")
 }
 
 const (
@@ -87,8 +88,10 @@ var _ = BeforeSuite(func() {
 	err = root.CreateNamespace()
 	Expect(err).NotTo(HaveOccurred())
 
-	stopCh := genericapiserver.SetupSignalHandler()
-	go root.RunOperatorAndServer(kubeconfigPath, stopCh)
+	if !framework.SelfHostedOperator {
+		stopCh := genericapiserver.SetupSignalHandler()
+		go root.RunOperatorAndServer(kubeconfigPath, stopCh)
+	}
 
 	root.EventuallyCRD().Should(Succeed())
 	root.EventuallyAPIServiceReady().Should(Succeed())
