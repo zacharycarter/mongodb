9238ad2eb0fbb97d6def4ee29c2821024239e23e
the-redback <maruf.2hin@gmail.com>
2018-07-23 11:48:47 +0600

    shfmtshfmt -i 2 -ci -w -l ./hack/

--- hack/libbuild/common/lib.sh
+++ hack/libbuild/common/lib.sh
@@ -28,7 +28,8 @@ clone() {
   fi
   echo -e "\n"
   local cmd="git clone $url $folder"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
 
 # http://stackoverflow.com/a/36979153/244009
@@ -39,13 +40,17 @@ checkout() {
   fi
   inside_git_repo
   local cmd="git clean -xfd"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="git fetch --all --prune"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="git fetch --tags"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="git checkout -f -B $1 origin/$1"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
 
 apply_tag() {
@@ -55,9 +60,11 @@ apply_tag() {
   fi
   inside_git_repo
   local cmd="git tag -fa $1"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="git push --tag -f"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
 
 # Based on metadata() func in config.py
@@ -111,17 +118,21 @@ EOL
 
 build() {
   local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
 
 attic_up() {
   local cmd="docker tag $DOCKER_REGISTRY/$IMG:$TAG gcr.io/$GCR_PROJECT/$IMG:$TAG"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="gcloud docker -- push gcr.io/$GCR_PROJECT/$IMG:$TAG"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 
   local cmd="docker tag $DOCKER_REGISTRY/$IMG:$TAG docker.appscode.com/$IMG:$TAG"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="docker push docker.appscode.com/$IMG:$TAG"
   echo $cmd
   until $cmd; do echo "Try again"; done
@@ -129,7 +140,8 @@ attic_up() {
 
 hub_up() {
   local cmd="docker push $DOCKER_REGISTRY/$IMG:$TAG"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
 
 hub_canary() {
@@ -148,16 +160,20 @@ hub_canary() {
 
 attic_pull() {
   local cmd="docker pull docker.appscode.com/$IMG:$TAG"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="docker tag docker.appscode.com/$IMG:$TAG $DOCKER_REGISTRY/$IMG:$TAG"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
 
 gcr_pull() {
   local cmd="gcloud docker -- pull gcr.io/$GCR_PROJECT/$IMG:$TAG"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="docker tag gcr.io/$GCR_PROJECT/$IMG:$TAG $DOCKER_REGISTRY/$IMG:$TAG"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
 
 docker_release() {
@@ -165,17 +181,25 @@ docker_release() {
 }
 
 docker_check() {
-  name=$IMG-$(date +%s | sha256sum | base64 | head -c 8; echo)
+  name=$IMG-$(
+    date +%s | sha256sum | base64 | head -c 8
+    echo
+  )
   local cmd="docker run -d -P -it --name=$name $DOCKER_REGISTRY/$IMG:$TAG"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="docker exec -it $name ps aux"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="sleep 5"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="docker exec -it $name ps aux"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="docker stop $name && docker rm $name"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
 
 docker_run() {
@@ -183,7 +207,10 @@ docker_run() {
   if [ $# -eq 1 ]; then
     img=$1
   fi
-  name=$img-$(date +%s | sha256sum | base64 | head -c 8; echo)
+  name=$img-$(
+    date +%s | sha256sum | base64 | head -c 8
+    echo
+  )
   privileged="${PRIVILEGED_CONTAINER:-}"
   net="--net=host"
   extra_opts="-v $PWD/pv:/var/pv"
@@ -191,7 +218,8 @@ docker_run() {
   echo pv >.gitignore
   mkdir -p pv
   local cmd="docker run -d -P -it $privileged $net $extra_opts --name=$name $DOCKER_REGISTRY/$img:$TAG $docker_cmd"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
 
 docker_sh() {
@@ -199,25 +227,33 @@ docker_sh() {
   if [ $# -eq 1 ]; then
     img=$1
   fi
-  name=$img-$(date +%s | sha256sum | base64 | head -c 8; echo)
+  name=$img-$(
+    date +%s | sha256sum | base64 | head -c 8
+    echo
+  )
   privileged="${PRIVILEGED_CONTAINER:-}"
   net="${DOCKER_NETWORK:-}"
   extra_opts="${EXTRA_DOCKER_OPTS:-}"
   local cmd="docker run -d -P -it $privileged $net $extra_opts --name=$name $DOCKER_REGISTRY/$img:$TAG"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   cmd="docker exec -it $name bash"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
 
 docker_rm() {
   local cmd="docker stop $(docker ps)"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
   local cmd="docker rm -f $(docker ps -a)"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
 
 docker_rmi() {
   docker_rm || true
   local cmd="docker rmi -f $(docker images -a)"
-  echo $cmd; $cmd
+  echo $cmd
+  $cmd
 }
